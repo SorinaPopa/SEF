97d665baccdd6ead398caf2eb8b96fdf921765e3
Sorina <sorinapopaan@gmail.com>
2022-05-22 22:49:57 +0300

    Navigation Drawer Button

--- STADEVENTS/app/src/main/java/com/example/stadevents/MainActivity.kt
+++ STADEVENTS/app/src/main/java/com/example/stadevents/MainActivity.kt
@@ -2,10 +2,56 @@ package com.example.stadevents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
+import com.example.stadevents.databinding.ActivityMainBinding
+import com.example.stadevents.home.HomeViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfig: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupBottomNavigation()
+        setupNavigationDrawer()
+        setupAppBarConfig()
+    }
+
+    private fun setupBottomNavigation() {
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+    private fun setupNavigationDrawer() {
+        binding.navigationDrawer.setupWithNavController(navController)
+    }
+
+    private fun setupAppBarConfig() {
+        appBarConfig =
+            AppBarConfiguration(setOf(R.id.homeFragment, R.id.searchFragment), binding.drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfig)
     }
 }
---
+++ STADEVENTS/app/src/main/java/com/example/stadevents/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.stadevents
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
