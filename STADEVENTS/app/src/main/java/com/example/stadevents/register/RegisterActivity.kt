e21b6dedf66136b3ae672f582c2dae7fb7d1d737
Sorina <sorinapopaan@gmail.com>
2022-05-25 18:04:22 +0300

    Set OnClickListener for Register Button

--- STADEVENTS/app/src/main/java/com/example/stadevents/register/RegisterActivity.kt
+++ STADEVENTS/app/src/main/java/com/example/stadevents/register/RegisterActivity.kt
@@ -1,12 +1,42 @@
 package com.example.stadevents.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.viewModels
 import com.example.stadevents.R
+import com.example.stadevents.databinding.LoginActivityBinding
+import com.example.stadevents.databinding.RegisterActivityBinding
+import com.example.stadevents.login.LoginActivity
+import com.example.stadevents.login.LoginViewModel
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: RegisterActivityBinding
+    private val registerViewModel: RegisterViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_activity)
+        binding = RegisterActivityBinding.inflate(layoutInflater)
+        binding.lifecycleOwner = this
+        binding.registerViewModel = registerViewModel
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        subscribeToObserver()
+    }
+
+    private fun subscribeToObserver() {
+        registerViewModel.onRegisterClicked.observe(this) { value ->
+            if (value) {
+                val registerHere: Button = findViewById(R.id.register)
+                registerHere.setOnClickListener {
+                    val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+
+            }
+        }
     }
 }
--- STADEVENTS/app/src/main/java/com/example/stadevents/register/RegisterViewModel.kt
+++ STADEVENTS/app/src/main/java/com/example/stadevents/register/RegisterViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.stadevents.register
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class RegisterViewModel : ViewModel() {
+
+    val onRegisterClicked = MutableLiveData(false)
+
+    fun onClickRegisterButton(){
+        onRegisterClicked.value = true
+    }
 }
